e59596aa6e5c9ca73186a7338f12698ae6f80755
kevinalfito69 <kevinalfito69@gmail.com>
2022-09-29 01:16:43 +0700

    Add basic template and testing Js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,13 @@
-import 'regenerator-runtime'; /* for async await transpile */
-import '../styles/main.css';
+import "regenerator-runtime"; /* for async await transpile */
+import "../styles/main.css";
+import data from "../DATA.json";
 
-console.log('Hello Coders! :)');
+document.addEventListener("DOMContentLoaded", () => {
+  const restaurantList = document.querySelector(".restaurant__list");
+  data.restaurants.forEach((resto) => {
+    restaurantList.innerHTML += `
+       <p> ${resto.id} </p>
+    `;
+  });
+});
+console.log("Hello Coders! :)");
