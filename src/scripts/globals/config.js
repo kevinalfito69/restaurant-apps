66b1a36b4fc3ff455185f22c667e3fac16fe5329
kevinalfito69 <kevinalfito69@gmail.com>
2022-11-29 16:45:50 +0700

    Setup Idb

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -3,5 +3,8 @@ const CONFIG = {
   SMALL_IMG_URL: 'https://restaurant-api.dicoding.dev/images/small/',
   MEDIUM_IMG_URL: 'https://restaurant-api.dicoding.dev/images/medium/',
   LARGE_IMG_URL: 'https://restaurant-api.dicoding.dev/images/large/',
+  DATABASE_NAME: 'resto-database',
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: 'resto',
 };
 export default CONFIG;
